b38b1025a53b2a8c8dbb4d24ddedb75c5568a1fc
Julio Faerman <julio@faermanj.com>
2025-09-13 11:24:20 +0000

    wip

--- scripts/aws-janitor.sh
+++ scripts/aws-janitor.sh
@@ -2,8 +2,10 @@
 set -e 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+$DIR/s3-janitor.sh
+
 $DIR/cloudformation-janitor.sh
+
 $DIR/ec2-janitor.sh
 $DIR/lambda-janitor.sh
-$DIR/s3-janitor.sh
 
