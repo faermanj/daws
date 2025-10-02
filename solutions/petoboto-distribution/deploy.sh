8fc3b73eee36c722fc93bdc57a4a4eb56f6322ae
Julio Faerman <356476+faermanj@users.noreply.github.com>
2025-10-02 22:34:09 +0200

    feat: enhance CloudFormation templates and deploy scripts for Petoboto API and distribution

--- solutions/petoboto-distribution/deploy.sh
+++ solutions/petoboto-distribution/deploy.sh
@@ -1,40 +1,56 @@
 #!/bin/bash 
 set -e
 DIR=$(cd $(dirname $0) && pwd)
-pushd $DIR/..
+pushd $DIR/../..
 
 ENV_ID=${ENV_ID:-"project"}
 DOMAIN_NAME=${DOMAIN_NAME:-"petoboto.com"}
 ZONE_ID=${ZONE_ID:-"Z01386901AXGFXHXKIDJX"}
+DB_PASSWORD=${DB_PASSWORD:-"Masterkey123"}
 
-aws cloudformation deploy --stack-name acm-simple \
-    --parameter-overrides HostedZoneId=$ZONE_ID \
+aws cloudformation deploy \
+    --stack-name acm-simple \
+    --template-file cloudformation/acm-simple/template.cform.yaml \
+    --parameter-overrides \
+        HostedZoneId=$ZONE_ID \
         DomainName=$DOMAIN_NAME \
-    --template-file $DIR/../acm-simple/template.cform.yaml
 
 aws cloudformation deploy \
     --stack-name petoboto-resources \
     --template-file solutions/petoboto-resources/bucket.cform.yaml
 BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name petoboto-resources --query "Stacks[0].Outputs[?OutputKey=='BucketName'].OutputValue" --output text)
 echo BUCKET_NAME=$BUCKET_NAME
-aws s3 cp solutions/petoboto-resources/src/ s3://$BUCKET_NAME/ --acl public-read
+aws s3 sync ./solutions/petoboto-resources/src/ s3://$BUCKET_NAME/ --acl public-read
 
+aws cloudformation deploy \
+    --stack-name vpc-3ha \
+    --template-file cloudformation/vpc-3ha/template.cform.yaml
+aws cloudformation deploy \
+    --stack-name rds-mysql-sls \
+    --template-file cloudformation/rds-mysql-sls/template.cform.yaml \
+    --parameter-overrides DBPassword=$DB_PASSWORD
 
-aws cloudformation deploy --stack-name vpc-3ha --template-file cloudformation/vpc-3ha/template.cform.yaml
-aws cloudformation deploy --stack-name rds-mysql-sls --template-file cloudformation/rds-mysql-sls/template.cform.yaml --parameter-overrides DBPassword=Masterkey123
-mvn -f solutions/petoboto-api-fn clean verify
-sam deploy --stack-name petoboto-api-fn --template-file solutions/petoboto-api-fn/sam.cform.yaml --capabilities CAPABILITY_IAM --resolve-s3
+mvn -f ./solutions/petoboto-api-fn clean verify
+sam deploy \
+    --stack-name petoboto-api-fn \
+    --template-file solutions/petoboto-api-fn/sam.cform.yaml \
+    --capabilities CAPABILITY_IAM \
+    --resolve-s3
 API_URL=$(aws cloudformation describe-stacks --stack-name petoboto-api-fn --query "Stacks[0].Outputs[?OutputKey=='PetobotoApiUrl'].OutputValue" --output text)
 echo $API_URL
 
 
 aws cloudformation deploy \
-    --stack-name cloudfront \
-    --template-file cloudformation/cloudfront/template.cform.yaml
-DISTRIBUTION_DOMAIN=$(aws cloudformation describe-stacks --stack-name cloudfront --query "Stacks[0].Outputs[?OutputKey=='DistributionDomainName'].OutputValue" --output text)
+    --stack-name petoboto-distribution \
+    --template-file solutions/petoboto-distribution/template.cform.yaml
+DISTRIBUTION_DOMAIN=$(aws cloudformation describe-stacks \
+    --stack-name petoboto-distribution \
+    --query "Stacks[0].Outputs[?OutputKey=='DistributionDomainName'].OutputValue" \
+    --output text)
 echo "http://$DISTRIBUTION_DOMAIN/"
 
-aws cloudformation deploy --stack-name petoboto-distribution \
+aws cloudformation deploy \
+    --stack-name petoboto-alias \
     --parameter-overrides \
         HostedZoneId=$ZONE_ID \
         DomainName=$DOMAIN_NAME \
@@ -46,4 +62,4 @@ echo time curl -s "http://$HOST/tuna-bigfile.jpg" -o /dev/null
 echo ab -k -t 333 -c 22 -r "http://$HOST/tuna-bigfile.jpg"
 
 popd
-echo "Done.
+echo "Done."
