b2b2101adc83284eed8cb7f7d25be3409e731f1e
Julio Faerman <356476+faermanj@users.noreply.github.com>
2025-10-07 12:46:43 +0200

    Refactor CloudFormation templates and deployment scripts for Petoboto project, including removal of unused files, updates to alias and health check configurations, and introduction of weighted routing for static and API resources.

--- solutions/petoboto-color/deploy-service.sh
+++ solutions/petoboto-color/deploy-service.sh
@@ -57,49 +57,6 @@ aws cloudformation deploy \
 API_URL=$(aws cloudformation describe-stacks --stack-name petoboto-api-fn-$COLOR --query "Stacks[0].Outputs[?OutputKey=='PetobotoApiUrl'].OutputValue" --output text)
 echo $API_URL
 
-# Distribution and Alias
-aws cloudformation deploy \
-    --stack-name petoboto-color-$COLOR \
-    --parameter-overrides \
-        DomainName=$DOMAIN_NAME \
-        EnvId=$ENV_ID \
-        Color=$COLOR \
-    --template-file solutions/petoboto-color/template.cform.yaml
-DISTRIBUTION_DOMAIN=$(aws cloudformation describe-stacks \
-    --stack-name petoboto-color-$COLOR \
-    --query "Stacks[0].Outputs[?OutputKey=='DistributionDomainName'].OutputValue" \
-    --output text)
-echo "http://$DISTRIBUTION_DOMAIN/"
-DISTRIBUTION_ID=$(aws cloudformation describe-stacks \
-    --stack-name petoboto-color-$COLOR \
-    --query "Stacks[0].Outputs[?OutputKey=='DistributionId'].OutputValue" \
-    --output text)
-aws cloudformation deploy \
-    --stack-name petoboto-alias-$COLOR \
-    --template-file solutions/petoboto-color/alias.cform.yaml \
-    --parameter-overrides \
-        HostedZoneId=$ZONE_ID \
-        Color=$COLOR \
-        DomainName=$DOMAIN_NAME \
-        EnvId=$ENV_ID
-aws cloudformation deploy \
-    --stack-name petoboto-healthcheck-$COLOR \
-    --template-file solutions/petoboto-color/healthcheck.cform.yaml \
-
-    --parameter-overrides \
-        EnvId=$ENV_ID \
-        Color=$COLOR \
-        DomainName=$DOMAIN_NAME \
-
-aws cloudfront create-invalidation \
-    --distribution-id $DISTRIBUTION_ID \
-    --paths "/*"
-
-# Suggested Tests
-HOST=${ENV_ID}-${COLOR}.${DOMAIN_NAME}
-echo "http://$HOST/tuna-1mb.jpg"
-echo time curl -s "http://$HOST/tuna-1mb.jpg" -o /dev/null
-echo ab -k -n 2048 -c 16 -r "http://$HOST/tuna-1mb.jpg"
 
 popd
 echo "Done."
--- solutions/petoboto-color/deploy.sh
+++ solutions/petoboto-color/deploy.sh
@@ -7,21 +7,63 @@ COLOR_B=${2:-"green"}
 DOMAIN_NAME=${DOMAIN_NAME:-"petoboto.com"}
 ZONE_ID=${ZONE_ID:-"Z01386901AXGFXHXKIDJX"}
 ENV_ID=${ENV_ID:-"project"}
-WEIGHT_A=${WEIGHT_A:-50}
-WEIGHT_B=${WEIGHT_B:-50}
+WEIGHT_A=${WEIGHT_A:-80}
+WEIGHT_B=${WEIGHT_B:-20}
 
 source $DIR/deploy-foundation.sh
 source $DIR/deploy-service.sh "$COLOR_A"
 source $DIR/deploy-service.sh "$COLOR_B"
 
 aws cloudformation deploy \
-    --stack-name petoboto-apex \
+    --stack-name petoboto-weights \
+    --template-file solutions/petoboto-color/color-weights.cform.yaml \
     --parameter-overrides \
-        EnvId=$ENV_ID \
-        DomainName=$DOMAIN_NAME \
-        HostedZoneId=$ZONE_ID \
+        EnvId="$ENV_ID" \
+        DomainName="$DOMAIN_NAME" \
+        HostedZoneId="$ZONE_ID" \
         ColorA="$COLOR_A" \
         ColorB="$COLOR_B" \
-        WeightA=$WEIGHT_A \
-        WeightB=$WEIGHT_B \
-    --template-file solutions/petoboto-color/apex.cform.yaml
+        WeightA="$WEIGHT_A" \
+        WeightB="$WEIGHT_B"
+
+# Distribution and Alias
+aws cloudformation deploy \
+    --stack-name petoboto-distribution \
+    --template-file solutions/petoboto-color/template.cform.yaml \
+    --parameter-overrides \
+        DomainName=$DOMAIN_NAME \
+        EnvId=$ENV_ID
+DISTRIBUTION_DOMAIN=$(aws cloudformation describe-stacks \
+    --stack-name petoboto-distribution \
+    --query "Stacks[0].Outputs[?OutputKey=='DistributionDomainName'].OutputValue" \
+    --output text)
+echo "http://$DISTRIBUTION_DOMAIN/"
+DISTRIBUTION_ID=$(aws cloudformation describe-stacks \
+    --stack-name petoboto-distribution \
+    --query "Stacks[0].Outputs[?OutputKey=='DistributionId'].OutputValue" \
+    --output text)
+aws cloudformation deploy \
+    --stack-name petoboto-alias \
+    --template-file solutions/petoboto-color/alias.cform.yaml \
+    --parameter-overrides \
+        HostedZoneId=$ZONE_ID \
+        DomainName=$DOMAIN_NAME \
+        EnvId=$ENV_ID
+
+aws cloudformation deploy \
+    --stack-name petoboto-healthcheck \
+    --template-file solutions/petoboto-color/healthcheck.cform.yaml \
+    --parameter-overrides \
+        EnvId=$ENV_ID \
+        Color=$COLOR \
+        DomainName=$DOMAIN_NAME
+
+aws cloudfront create-invalidation \
+    --distribution-id $DISTRIBUTION_ID \
+    --paths "/*"
+
+# Suggested Tests
+HOST=${ENV_ID}-${COLOR}.${DOMAIN_NAME}
+echo "http://$HOST/tuna-1mb.jpg"
+echo time curl -s "http://$HOST/tuna-1mb.jpg" -o /dev/null
+echo ab -k -n 2048 -c 16 -r "http://$HOST/tuna-1mb.jpg"
