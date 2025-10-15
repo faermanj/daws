eb46be860e1c628c3a789f742a2b8e855c29693d
Julio Faerman <julio@faermanj.com>
2025-10-15 13:45:26 +0200

    Add CloudFormation templates and janitor scripts for Petoboto API deployment
    - Created domain-color.cform.yaml for API domain mapping with Route53 and API Gateway.
    - Added sam-color.cform.yaml for serverless function deployment with AWS SAM.
    - Introduced janitor scripts for cleaning up AWS resources:
      - aws-janitor.sh: Main script to invoke other janitor scripts.
      - cloudformation-janitor.sh: Deletes CloudFormation stacks.
      - cloudfront-janitor.sh: Manages CloudFront distributions.
      - ec2-janitor.sh: Cleans up EC2 instances and related resources.
      - janitor-s3.sh: Deletes S3 buckets and their contents.
      - lambda-janitor.sh: Deletes Lambda functions and layers.
      - route53-janitor.sh: Cleans up Route53 hosted zones and records.
      - s3-empty.sh: Empties S3 buckets without deleting them.
      - s3-janitor.sh: Deletes S3 buckets after emptying them.
    - Added alias.cform.yaml for ALB alias record creation in Route53.
    - Created cd.cform.yaml for a continuous deployment pipeline for CloudFront distribution.
    - Added deploy.sh script for deploying the entire Petoboto infrastructure.
    - Introduced healthcheck.cform.yaml for Route53 HTTPS health checks.
    - Created template.cform.yaml for CloudFront distribution with S3 origin.

---
+++ solutions/petoboto-distribution-cd/deploy.sh
@@ -0,0 +1,97 @@
+#!/bin/bash 
+set -e
+DIR=$(cd $(dirname $0) && pwd)
+pushd $DIR/../..
+
+ENV_ID=${ENV_ID:-"project"}
+DOMAIN_NAME=${DOMAIN_NAME:-"petoboto.com"}
+ZONE_ID=${ZONE_ID:-"Z01386901AXGFXHXKIDJX"}
+DB_PASSWORD=${DB_PASSWORD:-"Masterkey123"}
+
+# TLS Certificate
+aws cloudformation deploy \
+    --stack-name acm-simple \
+    --template-file cloudformation/acm-simple/template.cform.yaml \
+    --parameter-overrides \
+        HostedZoneId=$ZONE_ID \
+        DomainName=$DOMAIN_NAME \
+        EnvId=$ENV_ID
+
+# Resources Bucket
+aws cloudformation deploy \
+    --stack-name petoboto-resources \
+    --template-file solutions/petoboto-resources/bucket.cform.yaml \
+    --parameter-overrides EnvId=$ENV_ID
+BUCKET_NAME=$(aws cloudformation describe-stacks \
+    --stack-name petoboto-resources \
+    --query "Stacks[0].Outputs[?OutputKey=='ResourcesBucketName'].OutputValue" \
+    --output text)
+echo BUCKET_NAME=$BUCKET_NAME
+aws s3 sync ./solutions/petoboto-resources/src/ s3://$BUCKET_NAME/ 
+
+# VPC, Lambda and API
+aws cloudformation deploy \
+    --stack-name vpc-3ha \
+    --template-file cloudformation/vpc-3ha/template.cform.yaml \
+    --parameter-overrides EnvId=$ENV_ID
+aws cloudformation deploy \
+    --stack-name rds-mysql-sls \
+    --template-file cloudformation/rds-mysql-sls/template.cform.yaml \
+    --parameter-overrides DBPassword=$DB_PASSWORD EnvId=$ENV_ID
+
+mvn -f ./solutions/petoboto-api-fn clean verify
+sam deploy \
+    --stack-name petoboto-api-fn \
+    --template-file solutions/petoboto-api-fn/sam.cform.yaml \
+    --capabilities CAPABILITY_IAM \
+    --resolve-s3 \
+    --force-upload \
+    --no-confirm-changeset \
+    --no-fail-on-empty-changeset \
+    --parameter-overrides EnvId=$ENV_ID
+aws cloudformation deploy \
+    --stack-name petoboto-api-domain \
+    --template-file solutions/petoboto-api-fn/domain.cform.yaml \
+    --parameter-overrides \
+        HostedZoneId=$ZONE_ID \
+        DomainName=$DOMAIN_NAME \
+        EnvId=$ENV_ID
+API_URL=$(aws cloudformation describe-stacks --stack-name petoboto-api-fn --query "Stacks[0].Outputs[?OutputKey=='PetobotoApiUrl'].OutputValue" --output text)
+echo $API_URL
+
+# Distribution and Alias
+aws cloudformation deploy \
+    --stack-name petoboto-distribution \
+    --parameter-overrides \
+        DomainName=$DOMAIN_NAME \
+        EnvId=$ENV_ID \
+    --template-file solutions/petoboto-distribution/template.cform.yaml
+DISTRIBUTION_DOMAIN=$(aws cloudformation describe-stacks \
+    --stack-name petoboto-distribution \
+    --query "Stacks[0].Outputs[?OutputKey=='DistributionDomainName'].OutputValue" \
+    --output text)
+echo "http://$DISTRIBUTION_DOMAIN/"
+DISTRIBUTION_ID=$(aws cloudformation describe-stacks \
+    --stack-name petoboto-distribution \
+    --query "Stacks[0].Outputs[?OutputKey=='DistributionId'].OutputValue" \
+    --output text)
+aws cloudformation deploy \
+    --stack-name petoboto-alias \
+    --parameter-overrides \
+        HostedZoneId=$ZONE_ID \
+        DomainName=$DOMAIN_NAME \
+        EnvId=$ENV_ID \
+    --template-file solutions/petoboto-distribution/alias.cform.yaml
+
+aws cloudfront create-invalidation \
+    --distribution-id $DISTRIBUTION_ID \
+    --paths "/*"
+
+# Suggested Tests
+HOST=${ENV_ID}.${DOMAIN_NAME}
+echo "http://$HOST/tuna-1mb.jpg"
+echo time curl -s "http://$HOST/tuna-1mb.jpg" -o /dev/null
+echo ab -k -n 655350 -c 128 -r "http://$HOST/tuna-1mb.jpg"
+
+popd
+echo "Done."
